9dfb8c1c0d540960f7b3e306ef9f99b0555feb24
Adham Gad <adhamgad1@gmail.com>
2015-06-28 00:52:29 +0200

    Initial Commit

---
+++ دعوات قطر/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  دعوات قطر
+//
+//  Created by Adham Gad on 28,6//15.
+//  Copyright (c) 2015 Bixls. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ دعوات قطر/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  دعوات قطر
+//
+//  Created by Adham Gad on 28,6//15.
+//  Copyright (c) 2015 Bixls. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
