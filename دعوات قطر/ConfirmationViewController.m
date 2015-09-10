6eb8c94a28797ad93ad4f9b3e84412b0d5fe194b
Adham Gad <adhamgad1@gmail.com>
2015-09-10 10:24:33 +0200

    refactoring

--- دعوات قطر/ConfirmationViewController.m
+++ دعوات قطر/ConfirmationViewController.m
@@ -78,65 +78,6 @@
     }
 }
 
-//-(void)postRequest:(NSDictionary *)postDict{
-//    
-//    NSString *authStr = [NSString stringWithFormat:@"%@:%@", @"admin", @"admin"];
-//    NSData *authData = [authStr dataUsingEncoding:NSUTF8StringEncoding];
-//    NSString *authValue = [NSString stringWithFormat:@"Basic %@", [authData base64EncodedStringWithOptions:0]];
-//    NSString *urlString = @"http://bixls.com/Qatar/" ;
-//    NSURL *url = [NSURL URLWithString:urlString];
-//    
-//    ASIHTTPRequest *request = [ASIHTTPRequest requestWithURL:url];
-//    request.delegate = self;
-//    request.username =@"admin";
-//    request.password = @"admin";
-//    [request setRequestMethod:@"POST"];
-//    [request addRequestHeader:@"Authorization" value:authValue];
-//    [request addRequestHeader:@"Accept" value:@"application/json"];
-//    [request addRequestHeader:@"content-type" value:@"application/json"];
-//    request.allowCompressedResponse = NO;
-//    request.useCookiePersistence = NO;
-//    request.shouldCompressRequestBody = NO;
-//    [request setPostBody:[NSMutableData dataWithData:[NSJSONSerialization dataWithJSONObject:postDict options:kNilOptions error:nil]]];
-//    [request startAsynchronous];
-//    
-//    
-//}
-//
-//- (void)requestFinished:(ASIHTTPRequest *)request
-//{
-//    // Use when fetching text data
-//    NSString *responseString = [request responseString];
-//    NSLog(@"%@",responseString);
-//    
-//    // Use when fetching binary data
-//    NSData *responseData = [request responseData];
-//    self.responseDictionary = [NSJSONSerialization JSONObjectWithData:responseData options:kNilOptions error:nil];
-//    if ([self.responseDictionary[@"success"] integerValue] == 0) {
-//        self.responseLabel.text = @"عفوا كودالتفعيل خطأ" ;
-//    }else if ([self.responseDictionary[@"success"] integerValue]== 1){
-//        self.responseLabel.text = @"شكراً لك تم تفعيل حسابك";
-//        [self.userDefaults setInteger:0 forKey:@"Guest"];
-//        [self.userDefaults setInteger:1 forKey:@"signedIn"];
-//        [self.userDefaults synchronize];
-//        self.activateFlag = 0;
-//        [self.userDefaults setInteger:self.activateFlag forKey:@"activateFlag"];
-//        [self dismissViewControllerAnimated:YES completion:nil];
-//        //[self.navigationController popToRootViewControllerAnimated:YES];
-//    }
-//    NSLog(@"%@",self.responseDictionary);
-// 
-//    
-//}
-//
-//- (void)requestFailed:(ASIHTTPRequest *)request
-//{
-//    NSError *error = [request error];
-//    NSLog(@"%@",error);
-//}
-//
-
-
 
 #pragma mark Textfield delegate methods
 
